32a7d699de6575f1b0d01bf74e59624fc62342db
mc <mc@localhost.localdomain>
2019-12-01 22:01:57 +0300

    remove logic to GuessNumber

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,9 +1,21 @@
+import java.util.*;
 
 public class GuessNumber {
+ 
+    Player playerOne;
+    Player playerTwo;
+    int secretNumber;
 
-    private int secretNumber = (int)(Math.random() * 10);
+    GuessNumber(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+   } 
+
+    Scanner scan = new Scanner(System.in);
+  
 
     public String chekNumber(int currentNumber) {
+         
         if (currentNumber > secretNumber) {
             System.out.println ("Введенное вами число больше загаданого компьютером");
             return "больше";
@@ -14,5 +26,33 @@ public class GuessNumber {
             System.out.println ("Вы угадали!");
             return "равно";
         }
+    }
+
+    public void play() {
+        String answer = "";
+        String result = "";
+        
+
+        do {
+            secretNumber = (int)(Math.random() * 10);
+            do {
+                System.out.print(playerOne.name + ", введите число: ");
+                playerOne.setNumber(scan.nextInt());
+                result = chekNumber(playerOne.getNumber());
+                if (result.equals("равно")) {
+                    break;
+                }
+               System.out.print(playerTwo.name + ", введите число: ");
+                playerTwo.setNumber(scan.nextInt());
+                result = chekNumber(playerTwo.getNumber());
+                if (result.equals("равно")) {
+                    break;
+                } 
+            } while (!result.equals("равно"));
+            do {
+                    System.out.print("Хотите продолжить? [Да/Нет]: ");
+                    answer = scan.next().trim().toLowerCase();
+            } while (!answer.equals("да") && !answer.equals("нет"));
+        } while (answer.equals("да"));
     }
 }
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -9,30 +9,8 @@ public class GuessNumberTest {
         Player playerOne = new Player(scan.nextLine());
         System.out.print("Введите имя второго игрока: ");
         Player playerTwo = new Player(scan.nextLine());
-        GuessNumber number = new GuessNumber();
+        GuessNumber number = new GuessNumber(playerOne, playerTwo);
 
-        String answer = "";
-        String result = "";
-
-        do {
-            do {
-                System.out.print(playerOne.name + ", введите число: ");
-                playerOne.setNumber(scan.nextInt());
-                result = number.chekNumber(playerOne.getNumber());
-                if (result.equals("равно")) {
-                    break;
-                }
-                System.out.print(playerTwo.name + ", введите число: ");
-                playerTwo.setNumber(scan.nextInt());
-                result = number.chekNumber(playerTwo.getNumber());
-                if (result.equals("равно")) {
-                    break;
-                } 
-            } while (!result.equals("равно"));
-            do {
-                    System.out.print("Хотите продолжить? [Да/Нет]: ");
-                    answer = scan.next().trim().toLowerCase();
-            } while (!answer.equals("да") && !answer.equals("нет"));
-        } while (answer.equals("да"));
+        number.play();
     } 
 }
