3cd4d0bbb8e6f16a0aee67c554b36fea7c866fa2
mc <mc@localhost.localdomain>
2019-12-03 10:35:04 +0300

    Перенесла логику 'Хотите пролоджить' в GNT

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -2,54 +2,47 @@ import java.util.*;
 
 public class GuessNumber {
 
-    Player playerOne;
-    Player playerTwo;
-    int secretNumber;
+    Scanner scan = new Scanner(System.in);
+
+    private Player playerOne;
+    private Player playerTwo;
+    private int secretNumber;
 
     GuessNumber(Player playerOne, Player playerTwo) {
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
     }
 
-    Scanner scan = new Scanner(System.in);
-
-    public String chekNumber(int currentNumber) {
-        if (currentNumber > secretNumber) {
-            System.out.println ("Введенное вами число больше загаданого компьютером");
-            return "больше";
-        } else if (currentNumber < secretNumber) {
-            System.out.println ("Введенное вами число меньше загаданого компьютером");
-            return "меньше";
-        } else {
-            System.out.println ("Вы угадали!");
-            return "равно";
-        }
-    }
-
     public void play() {
-        String answer = "";
-        String result = "";
+        boolean result = false;
+        secretNumber = (int)(Math.random() * 101);
 
         do {
-            secretNumber = (int)(Math.random() * 101);
-            do {
-                System.out.print(playerOne.getName() + ", введите число: ");
-                playerOne.setNumber(scan.nextInt());
-                result = chekNumber(playerOne.getNumber());
-                if (result.equals("равно")) {
-                    break;
-                }
-               System.out.print(playerTwo.getName() + ", введите число: ");
-                playerTwo.setNumber(scan.nextInt());
-                result = chekNumber(playerTwo.getNumber());
-                if (result.equals("равно")) {
-                    break;
-                }
-            } while (!result.equals("равно"));
-            do {
-                    System.out.print("Хотите продолжить? [Да/Нет]: ");
-                    answer = scan.next().trim().toLowerCase();
-            } while (!answer.equals("да") && !answer.equals("нет"));
-        } while (answer.equals("да"));
+            System.out.print(playerOne.getName() + ", введите число: ");
+            playerOne.setNumber(scan.nextInt());
+            result = checkNumber(playerOne.getNumber());
+            if (result) {
+                break;
+            }
+            System.out.print(playerTwo.getName() + ", введите число: ");
+            playerTwo.setNumber(scan.nextInt());
+            result = checkNumber(playerTwo.getNumber());
+            if (result) {
+                break;
+            }
+        } while (!result);
+    }
+
+    private boolean checkNumber(int currentNumber) {
+        if (currentNumber > secretNumber) {
+            System.out.println ("Введенное вами число больше загаданого компьютером");
+            return false;
+        } else if (currentNumber < secretNumber) {
+            System.out.println ("Введенное вами число меньше загаданого компьютером");
+            return false;
+        } else {
+            System.out.println ("Вы угадали!");
+            return true;
+        }
     }
 }
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -4,6 +4,7 @@ public class GuessNumberTest {
 
     public static void main(String[] arg) {
         Scanner scan = new Scanner(System.in);
+        String answer = "";
 
         System.out.print("Введите имя первого игрока: ");
         Player playerOne = new Player(scan.nextLine());
@@ -11,6 +12,12 @@ public class GuessNumberTest {
         Player playerTwo = new Player(scan.nextLine());
         GuessNumber number = new GuessNumber(playerOne, playerTwo);
 
-        number.play();
+        do {
+            number.play();
+            do {
+                System.out.print("Хотите продолжить? [Да/Нет]: ");
+                answer = scan.next().trim().toLowerCase();
+            } while (!"да".equals(answer) && !"нет".equals(answer));
+        } while ("да".equals(answer));
     }
 }
