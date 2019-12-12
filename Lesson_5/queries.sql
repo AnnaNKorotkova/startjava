1c38c20e87feaa8051545a0a17991ae53644e567
AnnaNKorotkova <korotkova.a.n@yandex.ru>
2019-12-12 20:36:23 +0300

    Lesson_5

---
+++ Lesson_5/queries.sql
@@ -0,0 +1,15 @@
+\c jaegersdb;
+
+SELECT * FROM jaegers;
+SELECT * FROM jaegers WHERE activeStatus IS TRUE;
+SELECT * FROM jaegers WHERE mark IS Mark-1 and Mark-2;
+SELECT * FROM jaegers WHERE mark = 'Mark-1' or mark = 'Mark-6';
+SELECT * FROM jaegers ORDER BY mark DESC;
+SELECT * FROM jaegers WHERE launch = (SELECT MIN(launch) FROM jaegers);
+SELECT * FROM jaegers WHERE kaijukill = (SELECT MAX(kaijukill) FROM jaegers) or 
+		kaijukill = (SELECT MIN(kaijukill) FROM jaegers) ORDER BY kaijukill DESC;
+SELECT AVG(weight) FROM jaegers;
+-- Для выполнения следующего пункта внесла доп. изменения
+UPDATE jaegers SET activeStatus = TRUE WHERE ID > 5;
+UPDATE jaegers SET kaijukill = kaijukill + 1 WHERE activeStatus = TRUE;
+DELETE FROM jaegers WHERE activeStatus = FALSE;
